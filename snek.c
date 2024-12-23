27f3d70ff3ef6a4c6c917acf460f70ba5e75d4f0
HmmmAha <hmmaha123456000@gmail.com>
2024-12-23 21:21:17 +0700

    Add files via upload

--- snek.c
+++ snek.c
@@ -1,35 +1,81 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <conio.h>
 #include <ctype.h>
 #include <windows.h>
+int n;
 
-int grid[5][5] = {0};
+
+
+void resetGrid();
 void moveSnake();
 void apple();
 void snakeLogic();
 void clearGrid();
 void putSnake();
 
-
-void putSnake()
+void snakeLogic(int grid[n][n], int n, int move)    //move: 1 = up, 2 = right, 3 = down, 4 = left
 {
-    int MIN = 1;
-    int MAX = 24;
+    
+}
 
-    // srand()
+void moveSnake(int grid[n][n], int n)
+{
+    if (kbhit())
+    {
+        switch (tolower(getch()))
+        {
+            case 'w':
+                snakeLogic(grid, n, 1);
+                break;
 
+            case 'd':
+                snakeLogic(grid, n, 2);
+                break;
 
+            case 's':
+                snakeLogic(grid, n, 3);
+                break;
+            
+            case 'a':
+                snakeLogic(grid, n, 4);
+                break;
+        }
+    }
 }
 
 
-void printGrid()
+void resetGrid(int grid[n][n], int n)
 {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            grid[i][j] = 0;
+        }
+    }
+}
+
+
+void putSnake(int n)
+{
+    srand(time(0));
+
+    int i = rand() % n;
+    int j = rand() % n;
+}
+
+
+void printGrid(int grid[n][n], int n)
+{
+    putSnake(n);
     while (1)
     {
-        for (int i = 0; i < 5; i++)
+        moveSnake(grid, n);
+        for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < n; j++)
             {
                 printf("%d ", grid[i][j]);
             }
@@ -43,7 +89,16 @@ void printGrid()
 
 int main()
 {
-    printGrid();
+    int n = 5;
+    int grid[n][n];
+    char request;
+    do
+    {
+        resetGrid(grid, n);
+
+        printGrid(grid, n);
+
+    } while (request == 'y' || request == 'Y');
     return 0;
 }
 
